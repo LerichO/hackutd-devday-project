8277742759344af43dd66f1e893824b61ec76fb8
LerichO <lerichyaso@gmail.com>
2023-09-09 15:11:17 -0500

    block 1 React fundamentals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './assets/react-logo.png';
+import Navbar from './components/Navbar';
+import VideoCard from './components/VideoCard.jsx';
 
 function App() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center">
-            <img className="animate-spin-slow" src={logo} alt="" />
+
+        <div className="min-h-screen w-screen overflow-x-hidden bg-primary text-text-primary">
+            <Navbar />
+            <h1 className="px-8 py-6 text-3xl font-bold">Anime List</h1>
+            <VideoCard />
         </div>
     );
 }
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,9 @@
+function VideoCard(){
+    return (
+        <div className="m-8 h-[299px] w-[180px] bg-secondary rounded-md">
+
+        </div>
+    )
+}
+
+export default VideoCard;
