8ef3ca03b9bd53963694deb640381e8eee80390d
LerichO <lerichyaso@gmail.com>
2023-09-09 16:10:09 -0500

    block 2 Design implementation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ function App() {
         <div className="min-h-screen w-screen overflow-x-hidden bg-primary text-text-primary">
             <Navbar />
             <h1 className="px-8 py-6 text-3xl font-bold">Anime List</h1>
-            <VideoCard />
+            <div>
+
+                <ul className="flex flex-wrap gap-32">
+                    <VideoCard
+                        title="Spunchbob Squarepants"
+                        thumbnailURL="https://m.media-amazon.com/images/I/812xdKnUlmL.jpg"
+                    />
+                    <VideoCard
+                        title="Avater: The Last Airbender"
+                        thumbnailURL="https://cdn11.bigcommerce.com/s-b72t4x/images/stencil/1280x1280/products/74409/81219/Avatar_the_Last_Airbender_Poster__47063.1488841748.jpg?c=2"
+                    />
+                    <VideoCard
+                        title="Cowboy Bebop"
+                        thumbnailURL="https://m.media-amazon.com/images/I/61eBpHDxJ2L._AC_UF894,1000_QL80_.jpg"
+                    />
+                    <VideoCard />
+                </ul>
+            </div>
         </div>
     );
 }
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,11 @@
-function VideoCard(){
+function VideoCard(props){
     return (
-        <div className="m-8 h-[299px] w-[180px] bg-secondary rounded-md">
-
+        <div className="relative m-8 h-[299px] w-[180px] bg-secondary rounded-md">
+            <img src={props.thumbnailURL} alt={props.title} className="h-full rounded-md"></img>
+            <div className="absolute bottom-0 w-full h-2/5 rounded-md bg-gradient-to-t from-black to-transparent"></div>
+            <h1 className="absolute bottom-4 left-4 font-semibold">
+                {props.title}
+            </h1>
         </div>
     )
 }
